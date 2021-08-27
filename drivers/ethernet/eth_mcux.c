819084e5ba635b570bdf9ac9af38b7a1f4f45530
Hake Huang <hake.huang@oss.nxp.com>
2021-08-27 15:22:52 +0800

    handle the timer interrupt in common isr
    Signed-off-by: Hake Huang <hake.huang@oss.nxp.com>

--- drivers/ethernet/eth_mcux.c
+++ drivers/ethernet/eth_mcux.c
@@ -1152,7 +1152,7 @@ static const struct ethernet_api api_funcs = {
 static void eth_mcux_ptp_isr(const struct device *dev)
 {
 	struct eth_context *context = dev->data;
-	printf("TS Timer\r\n");
+	LOG_INF("TS Timer\r\n");
 	ENET_TimeStampIRQHandler(context->base, &context->enet_handle);
 }
 #endif
@@ -1164,12 +1164,6 @@ static void eth_mcux_common_isr(const struct device *dev)
 	uint32_t EIR = ENET_GetInterruptStatus(context->base);
 	int irq_lock_key = irq_lock();
 
-	if (EIR) {
-		ENET_ClearInterruptStatus(context->base,
-		  ~(kENET_TxBufferInterrupt | kENET_TxFrameInterrupt
-		    | kENET_RxBufferInterrupt | kENET_RxFrameInterrupt
-		    | ENET_EIR_MII_MASK));
-	}
 	if (EIR & (kENET_RxBufferInterrupt | kENET_RxFrameInterrupt)) {
 		ENET_ReceiveIRQHandler(context->base, &context->enet_handle);
 	}
@@ -1180,7 +1174,15 @@ static void eth_mcux_common_isr(const struct device *dev)
 		k_work_submit(&context->phy_work);
 		ENET_ClearInterruptStatus(context->base, kENET_MiiInterrupt);
 	}
-
+	if (EIR & ENET_TS_INTERRUPT) {
+		ENET_TimeStampIRQHandler(context->base, &context->enet_handle);
+	}
+	if (EIR) {
+		ENET_ClearInterruptStatus(context->base,
+		  ~(kENET_TxBufferInterrupt | kENET_TxFrameInterrupt
+		    | kENET_RxBufferInterrupt | kENET_RxFrameInterrupt
+		    | ENET_EIR_MII_MASK | ENET_TS_INTERRUPT));
+	}
 	irq_unlock(irq_lock_key);
 }
 #endif
