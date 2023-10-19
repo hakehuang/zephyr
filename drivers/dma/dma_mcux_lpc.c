b524ceb1ea6040f548a4010176ecc1c183b10c3e
Yves Vandervennet <yves.vandervennet@nxp.com>
2023-10-19 16:45:48 -0400

    drivers: dma: fine grain reset of DMA channel data instead of memset(0)
    Signed-off-by: Yves Vandervennet <yves.vandervennet@nxp.com>

--- drivers/dma/dma_mcux_lpc.c
+++ drivers/dma/dma_mcux_lpc.c
@@ -260,6 +260,20 @@ static int dma_mcux_lpc_queue_descriptors(struct channel_data *data,
 	return 0;
 }
 
+static void dma_mcux_lpc_clear_channel_data(struct channel_data *data)
+{
+
+ 	data->dma_callback = NULL;
+ 	data->dir = 0;
+ 	data->src_inc = 0;
+ 	data->dst_inc = 0; 	
+	data->descriptors_queued = false;
+	data->num_of_descriptors = 0;
+	data->curr_descriptor = NULL;
+	data->width = 0;
+	
+	return;
+}
 
 /* Configure a channel */
 static int dma_mcux_lpc_configure(const struct device *dev, uint32_t channel,
@@ -430,7 +444,7 @@ static int dma_mcux_lpc_configure(const struct device *dev, uint32_t channel,
 		data = DEV_CHANNEL_DATA(dev, virtual_channel);
 	}
 
-	//memset(data, 0, sizeof(struct channel_data));
+	dma_mcux_lpc_clear_channel_data(data);
 
 	data->dir = config->channel_direction;
 	/* Save the increment values for the reload function */
@@ -445,10 +459,8 @@ static int dma_mcux_lpc_configure(const struct device *dev, uint32_t channel,
 
 	k_spinlock_key_t otrigs_key = k_spin_lock(&configuring_otrigs);
 
-	data->descriptors_queued = false;
-	data->num_of_descriptors = 0;
 	data->width = width;
-	data->curr_descriptor = NULL;
+
 	if (config->source_chaining_en || config->dest_chaining_en) {
 		/* Chaining is enabled */
 		if (!dev_config->otrig_base_address || !dev_config->itrig_base_address) {
